6260c7e23fb30a7d03b0bc3610f63ca7846087f5
Ritik Kumar <singhritik7032@gmail.com>
2026-02-17 16:49:34 +0530

    updated

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 "use client"
 
-import Header from "@/components/Header";
+import Header from "@/Components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"
-import Testimonial from "@/components/extra/Testimonial";
-import FaqSection from "@/components/FaqSection";
-import BlogHome from "@/components/BlogHome";
-import Contribution from "@/components/Contribution";
+import Testimonial from "@/Components/extra/Testimonial";
+import FaqSection from "@/Components/FaqSection";
+import BlogHome from "@/Components/BlogHome";
+import Contribution from "@/Components/Contribution";
 export default function Home() {
   return (
    <>
